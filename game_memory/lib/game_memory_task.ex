58855972da81227b9fbbec87c8c56df0ff0e67be
Alejandro Jose Tortolero Machado <atortole@bancolombia.com.co>
2023-09-12 07:32:17 -0500

    Actualizacion

--- game_memory/lib/game_memory_task.ex
+++ game_memory/lib/game_memory_task.ex
@@ -1,66 +1,75 @@
 defmodule GameMemory.MemoryTask do
   import GameMemory.MemoryUtils
 
-  defp letras_seleccionadas(mapa_alphabet) do
-    vocales = obtener_vocales(mapa_alphabet, [])
-    consonantes = obtener_consonantes(mapa_alphabet, [])
-    Enum.concat(vocales, consonantes)
+  @game_version :"Juego de Memoria V1.0"
+
+  @doc """
+   Para ejecutar el juego
+  1.- Ubicarnos en la carpeta lib \
+  2.- Ejecutar el siguiente comando "elixirc game_memory_utils.ex game_memory_task.ex"  \
+  3.- Ejecutar iex \
+  4.- En earlang ejecutar GameMemory.MemoryTask.init_game()
+  """
+
+  defp selected_letters(map_alphabet)  do
+    vocales = get_vowels(map_alphabet, [])
+    consonants = get_consonants(map_alphabet, [])
+    Enum.concat(vocales, consonants)
   end
 
   def init_game do
-    letras_seleccionadas = letras_seleccionadas(mapa_alphabet())
-    resuelto = cargar_tablero(letras_seleccionadas)
-    juego(resuelto, resuelto, "Jugador", 3, 0, 0)
+    IO.puts("#{@game_version}, Bienvenido")
+    username = IO.gets("Ingrese Nombre: ") |> String.trim
+    sel_letters = selected_letters(alphabet_map())
+    board_build = board()
+    solved = load_board(sel_letters)
+    game(board_build,solved, username, 3, 0, 0)
   end
 
-  defp juego(tablero_actual, tablero_resuelto, jugador, vidas, acc_v, acc_c) when vidas > 0 and acc_v < 3 and acc_c < 3 do
-    IO.puts("Jugador: #{jugador}")
-    IO.puts("Vidas: #{vidas}")
-    IO.puts("Vocales encontradas: #{acc_v}")
-    IO.puts("Consonantes encontradas: #{acc_c}")
-    IO.puts(tablero_actual)
+  defp game(board_on, solved_board, player, lifes, acc_v, acc_c) when lifes > 0 and acc_v < 3 and acc_c < 3  do
+    IO.puts("Jugador: #{player}")
+    IO.puts("Vidas: #{lifes}")
+    IO.puts("Vocales:  #{acc_v}")
+    IO.puts("Consonantes:  #{acc_c}")
+    IO.puts(board_on)
 
-    pareja_ingresada = IO.gets("Ingresa una pareja x,y: ")
-                       |> String.trim
-                       |> String.split(",")
-                       |> Enum.map(&String.to_integer/1)
-                       |> List.to_tuple
+    ing_pair = IO.gets("Ingrese un par x,y: ")
+               |> String.trim
+               |> String.split(",")
+               |> Enum.map(&String.to_integer/1)
+               |> List.to_tuple
 
-    pareja_ingresada_invertida = pareja_ingresada |> Tuple.to_list |> Enum.reverse |> List.to_tuple
+    {pair1, pair2} = raw_positions(Map.keys(solved_board),Map.values(solved_board))
+                     |> Enum.filter(fn {k,_v} -> k ==  elem(ing_pair, 0) or k == elem(ing_pair, 1) end) |> List.to_tuple
 
-    {pareja1, pareja2} = posiciones_originales(Map.keys(tablero_resuelto), Map.values(tablero_resuelto))
-                         |> Enum.filter(fn {k, _v} -> k == elem(pareja_ingresada, 0) or k == elem(pareja_ingresada, 1) end)
-                         |> List.to_tuple
+    IO.puts(reveal_cards(board_on, pair1, pair2))
 
-    IO.puts(revelar_cartas(tablero_actual, pareja1, pareja2))
+    if String.downcase(elem(pair1, 1)) == String.downcase(elem(pair2, 1)) do
 
-    case {pareja1, pareja2} do
-      {{_, _, :vocal, :no_encontrada}, {_, _, :vocal, :no_encontrada}} ->
-        IO.puts("¡Pareja de vocales correcta!")
-        tablero_actualizado = revelar_cartas(tablero_actual, pareja1, pareja2)
-        juego(tablero_actualizado, tablero_resuelto, jugador, vidas, acc_v + 1, acc_c)
+      case is_consonant_or_vowel(pair1) do
+        {:vocal} ->
+          updated_board = reveal_cards(board_on, pair1, pair2)
+          game(updated_board, solved_board, player, lifes, acc_v + 1, acc_c)
+        {:consonante}  ->
+          updated_board = reveal_cards(board_on, pair1, pair2)
+          game(updated_board, solved_board, player, lifes, acc_v, acc_c + 1)
+      end
 
-      {{_, _, :consonante, :no_encontrada}, {_, _, :consonante, :no_encontrada}} ->
-        IO.puts("¡Pareja de consonantes correcta!")
-        tablero_actualizado = revelar_cartas(tablero_actual, pareja1, pareja2)
-        juego(tablero_actualizado, tablero_resuelto, jugador, vidas, acc_v, acc_c + 1)
+    else
 
-      _ ->
-        IO.puts("¡Pareja incorrecta!")
-        juego(tablero_actual, tablero_resuelto, jugador, vidas - 1, acc_v, acc_c)
+      IO.puts("Incorrecto! Intente nuevamente.")
+      game(board_on, solved_board, player, lifes - 1, acc_v, acc_c)
     end
   end
 
-  defp juego(_, _, _, vidas, _, _) when vidas == 0, do: {:fin_de_juego, :terminado}
+  defp game(_, _, _, lifes, acc_v, acc_c) when lifes == 0, do: {:gameover, :finished}
 
-  defp juego(_, _, _, _, acc_v, acc_c) when acc_v + acc_c == 6, do: {:ganador}
+  defp game(_, _, _, _, acc_v, acc_c), do: {:winner}
 
-  defp juego(_, _, _, _, _, _), do: {:continuar}
+  defp reveal_cards(board_on, pair1, pair2) do
+    p1 = to_string(elem(pair1, 0))
+    p2 = to_string(elem(pair2, 0))
+    String.replace(board_on, "-"<>p1<>"-", elem(pair1, 1)) |> String.replace("-"<>p2<>"-", elem(pair2,1))
 
-  defp revelar_cartas(tablero_actual, pareja1, pareja2) do
-    p1 = to_string(elem(pareja1, 0))
-    p2 = to_string(elem(pareja2, 0))
-    String.replace(tablero_actual, "-" <> p1 <> "-", elem(pareja1, 1))
-    |> String.replace("-" <> p2 <> "-", elem(pareja2, 1))
   end
 end
--- game_memory/lib/game_memory_utils.ex
+++ game_memory/lib/game_memory_utils.ex
@@ -1,50 +1,69 @@
 defmodule GameMemory.MemoryUtils do
-  def mapa_alphabet do
+  def board() do
+    """
+    [ -1- ]  [ -2- ]  [ -3- ]  [ -4- ]
+    [ -5- ]  [ -6- ]  [ -7- ]  [ -8- ]
+    [ -9- ] [ -10- ] [ -11- ] [ -12- ]
+    """
+  end
+
+  def alphabet_map() do
     vocales = ["a", "e", "i", "o", "u"]
-    alfabeto =
-      for letra <- String.graphemes("abcdefghijklmnopqrstuvwxyz") do
-        case letra in vocales do
-          true -> {String.to_atom(letra), {String.upcase(letra), letra, :vocal, :no_encontrada}}
-          false -> {String.to_atom(letra), {String.upcase(letra), letra, :consonante, :no_encontrada}}
-        end
+    alphabet = for letter <- String.graphemes("abcdefghijklmnopqrstuvwxyz") do
+      case letter in vocales do
+        true -> {String.to_atom(letter), {String.upcase(letter), letter, :vocal, :notfound}}
+        false -> {String.to_atom(letter), {String.upcase(letter), letter, :consonante, :notfound}}
       end
+    end
 
-    Map.new(alfabeto)
+    Map.new(alphabet)
   end
 
-  def obtener_vocales(mapa_alphabet, vocales_encontradas) when length(vocales_encontradas) < 3 do
-    claves_vocales = Enum.filter(mapa_alphabet, fn {_, v} -> elem(v, 2) == :vocal end) |> Map.keys
+  def get_vowels(alpha_map, l_vowels) when length(l_vowels) < 3 do
+    claves_vocales = Enum.filter(alpha_map, fn{_k,v} -> elem(v,2) == :vocal end) |> Enum.into(%{}) |> Map.keys
     clave_v = Enum.random(claves_vocales)
-    valor_v = Map.get(mapa_alphabet, clave_v)
-    pareja = {elem(valor_v, 0), elem(valor_v, 1), elem(valor_v, 2), elem(valor_v, 3)}
-    vocales = unless Enum.member?(vocales_encontradas, pareja), do: [pareja | vocales_encontradas], else: vocales_encontradas
-    obtener_vocales(mapa_alphabet, vocales)
+    valor_v = Map.get(alpha_map, clave_v)
+    par = {elem(valor_v, 0), elem(valor_v,1), elem(valor_v,2), elem(valor_v,3)}
+    l_vocales = unless Enum.member?(l_vowels, par), do: [par | l_vowels], else: l_vowels
+    get_vowels(alpha_map, l_vocales)
   end
 
-  def obtener_vocales(_, vocales_encontradas), do: vocales_encontradas
+  def get_vowels(_, l_vowels), do: l_vowels
 
-  def obtener_consonantes(mapa_alphabet, consonantes_encontradas) when length(consonantes_encontradas) < 3 do
-    claves_consonantes = Enum.filter(mapa_alphabet, fn {_, v} -> elem(v, 2) == :consonante end) |> Map.keys
+  def get_consonants(alpha_map, l_conson) when length(l_conson) < 3 do
+    claves_consonantes= Enum.filter(alpha_map, fn{_k,v} -> elem(v,2) == :consonante end) |> Enum.into(%{}) |> Map.keys
     clave_c = Enum.random(claves_consonantes)
-    valor_c = Map.get(mapa_alphabet, clave_c)
-    pareja = {elem(valor_c, 0), elem(valor_c, 1), elem(valor_c, 2), elem(valor_c, 3)}
-    consonantes = unless Enum.member?(consonantes_encontradas, pareja), do: [pareja | consonantes_encontradas], else: consonantes_encontradas
-    obtener_consonantes(mapa_alphabet, consonantes)
+    valor_c = Map.get(alpha_map, clave_c)
+    par = {elem(valor_c, 0), elem(valor_c,1), elem(valor_c, 2), elem(valor_c, 3)}
+    l_consonantes = unless Enum.member?(l_conson, par), do: [par | l_conson], else: l_conson
+    get_consonants(alpha_map, l_consonantes)
   end
 
-  def obtener_consonantes(_, consonantes_encontradas), do: consonantes_encontradas
+  def get_consonants(_, l_conson), do: l_conson
 
-  def cargar_tablero(letras_seleccionadas) do
-    posiciones_pares = 1..12 |> Enum.to_list
-    posiciones_revueltas = Enum.shuffle(posiciones_pares)
+  def load_board(sel_letters) do
+    pos_pares = 1..12
+                |> Enum.to_list
+                |> Enum.shuffle
+                |> Enum.chunk_every(2)
+                |> Enum.map(fn [a, b] -> {a, b} end)
+                |> Enum.zip(sel_letters)
 
-    Enum.reduce(posiciones_revueltas, %{}, fn posicion, acc ->
-      {letra, letras_seleccionadas} = List.pop_at(letras_seleccionadas, 0)
-      {{posicion, acc}, letra}
-    end)
+    pos_pares |> Map.new
   end
 
-  def posiciones_originales(posiciones_pares, letras_seleccionadas) do
-    Enum.zip(posiciones_pares, letras_seleccionadas) |> Map.new()
+  def raw_positions(pos_pares, sel_letters) do
+    Enum.zip(
+      Enum.flat_map(pos_pares, fn {x, y} -> [x,y] end),
+      Enum.flat_map(sel_letters, fn {x, y, _w, _z} -> [x,y] end))
+    |> Map.new
+  end
+
+  def is_consonant_or_vowel(par) do
+    vowels = ["a", "e", "i", "o", "u"]
+    case String.downcase(elem(par,1)) in vowels do
+      true -> {:vocal}
+      false -> {:consonante}
+    end
   end
 end
